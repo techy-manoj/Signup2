dc1f80cb4add7bdc5df1f2824b3909557a206a10
manoj kumar <techy.manoj1947@gmail.com>
2018-06-27 14:19:30 +0530

    added button in welcome screen to go back

--- app/src/main/java/com/example/venom/signup/welcome/Welcome.java
+++ app/src/main/java/com/example/venom/signup/welcome/Welcome.java
@@ -1,8 +1,8 @@
 package com.example.venom.signup.welcome;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+//import android.support.v7.app.AppCompatActivity;
+//import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -17,6 +17,7 @@ public class Welcome extends Base {
 //        super.onCreate(savedInstanceState);
 //        setContentView(R.layout.activity_main);
 //    }
+
 
     @Override
     public int getLayout() {
